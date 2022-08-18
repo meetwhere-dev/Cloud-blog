015a114b08879bbc96ee63b22c00c000292a165d
Cloud <802028620@qq.com>
2022-08-18 14:29:28 +0800

    update: 更新备案

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="mt-10 mb-6 m-auto opacity-50 text-center">
-    <span class="text-sm">2022-PRESENT © Cloud</span>
+    <span class="text-sm">浙ICP备20022878号-1 © Cloud</span>
   </div>
 </template>
