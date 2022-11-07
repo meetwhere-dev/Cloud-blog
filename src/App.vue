32ff4421778aed3b8863e6f5b11307553d79d3ef
wanyunfeng <gnefnuy@live.com>
2022-11-07 16:19:22 +0800

    refactor: SEO titile description

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,9 @@
 // you can use this to manipulate the document head in any components,
 // they will be rendered correctly in the html results with vite-ssg
 useHead({
-  title: 'Vitesse',
+  title: 'Cloud Wan\'s Blog',
   meta: [
-    { name: 'description', content: 'Opinionated Vite Starter Template' },
+    { name: 'description', content: 'Cloud Wan\'s Blog, frontend engineer' },
     {
       name: 'theme-color',
       content: computed(() => isDark.value ? '#00aba9' : '#ffffff'),
