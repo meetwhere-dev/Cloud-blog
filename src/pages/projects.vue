24c7145bf33e4344964f60230dab2bcacf605711
Cloud <gnefnuy@live.com>
2024-02-08 11:25:53 +0800

    feat: add link to wechat report

--- src/pages/projects.vue
+++ src/pages/projects.vue
@@ -6,6 +6,12 @@ const content = ref<HTMLDivElement>()
 const { t } = useI18n()
 
 const projects = [{
+  name: 'Wechat Annual Report 2023',
+  link: '/wechat-annual-report/',
+  desc: '微信年报2023.',
+  date: '2024-02-07T08:00:00.000+00:00',
+  icon: 'i-carbon:report',
+}, {
   name: '2048',
   link: '/2048/index.html',
   desc: '小游戏2048.',
